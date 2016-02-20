6955896140ad3f84c3c068e1cf64ae2804a6f4f3
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-02-20 10:12:05 -0800

    Removed old file

--- ARIA/includes/aria_get_ids.php
+++
@@ -1,8 +0,0 @@
-<?php
-	require_once("class-aria-create-competition.php");
-	require_once("class-aria-music.php");
-	$teacher_fields = ARIA_Create_Competition::aria_teacher_field_id_array();
-	$music_fields = ARIA_Music::aria_music_field_id_array();
-	$all_fields = array_merge( $teacher_fields, $music_fields );
-	echo json_encode($all_fields);
-?>
