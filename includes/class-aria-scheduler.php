0000fb728796dd633176dcd0f240ed456b4dc739
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-03-08 18:27:42 -0800

    Added a blank scheduler object

---
+++ includes/class-aria-scheduler.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * The file that provides competition scheduling functionality.
+ *
+ * A class definition that includes attributes and functions that allow the
+ * festival chairman to schedule all students that are registered for a music
+ * competition.
+ *
+ * @link       http://wesleykepke.github.io/ARIA/
+ * @since      1.0.0
+ *
+ * @package    ARIA
+ * @subpackage ARIA/includes
+ */
+
+/**
+ * The competition scheduler class.
+ *
+ * @since      1.0.0
+ * @package    ARIA
+ * @subpackage ARIA/includes
+ * @author     KREW
+ */
+class ARIA_Scheduler {
+
+  /**
+   * This function is responsible for scheduling all of the students in a music competition.
+   */
+  public static function aria_create_schedule() {
+
+  }
+}
