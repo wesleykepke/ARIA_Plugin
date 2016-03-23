eccd32864391e5125ab3213a73f6d196aef1282f
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-03-23 16:46:25 -0700

    Scheduler object can now be cutomized for regular comp. or command

--- admin/scheduler/class-aria-scheduler.php
+++ admin/scheduler/class-aria-scheduler.php
@@ -28,56 +28,141 @@ require_once(ARIA_ROOT . "/admin/scheduler/class-aria-student.php");
 class Scheduler {
 
   /**
-   * The days of the music competition. The precise amount of days for a
-   * music competition will be specified by the festival chairman.
+   * The days of the music competition.
+   *
+   * The precise amount of days for a music competition will depend on whether
+   * or not the scheduler is for a regular competition or for a command
+   * performance.
    *
    * @since 1.0.0
    * @access private
-   * @var 	array 	$days 	The days of the music competition.
+   * @var	array $days	The days of the music competition.
    */
   private $days;
 
   /**
-   * The number of days for a particular music competition.
+   * The type of the music competition (either a regular competition or
+   * command performance).
    *
    * @since 1.0.0
    * @access private
-   * @var 	int 	$num_days 	The number of days of the music competition.
+   * @var	int $competition_type	The type of the competition.
    */
-  private $num_days;
-
-  /**
-   * The number of sections per day of a particular music competition.
-   *
-   * @since 1.0.0
-   * @access private
-   * @var 	int 	$num_sections_per_day 	The number of sections per day of the music competition.
-   */
-  private $num_time_blocks_per_day;
+  private $competition_type;
 
   /**
    * The constructor used to instantiate a new scheduler object.
    *
+   * Using the parameters passed to the constructor, a new scheduler object will
+   * be created.
+   *
+   * @param	int	$competition_type	The type of the competition.
+   *
    * @since 1.0.0
-   * @param	int 	$num_days 	The number of days for the music competition.
-   * @param	int 	$num_time_blocks_per_day 	The number time blocks per day of the music competition.
-   * @param	int 	$num_concurrent_sections 	The number of sections within the time blocks of the music competition.
+   * @author KREW
    */
-  function __construct($num_days, $num_time_blocks_per_day, $num_concurrent_sections) {
-    $this->num_days = $num_days;
-    $this->num_time_blocks_per_day = $num_time_blocks_per_day;
-    $this->days = new SplFixedArray($this->num_days);
-    for ($i = 0; $i < $this->num_days; $i++) {
-      $this->days[$i] = new SplFixedArray($this->num_time_blocks_per_day);
-      for ($j = 0; $j < $this->num_time_blocks_per_day; $j++) {
-        $this->days[$i][$j] = new TimeBlock($num_concurrent_sections);
+  function __construct($competition_type) {
+    // create the base structure depending on the type of competition
+    switch($competition_type) {
+      case REGULAR_COMP:
+        $this->competition_type = $competition_type;
+        $this->days = new SplFixedArray(REGULAR_COMP_NUM_DAYS);
+      break;
+
+      case COMMAND_COMP:
+        $this->competition_type = $competition_type;
+        $this->days = new SplFixedArray(COMMAND_COMP_NUM_DAYS);
+      break;
+
+      default:
+        $this->competition_type = null;
+      break;
+    }
+  }
+
+  /**
+   * This function will create the structure for a normal competition.
+   *
+   * Using the parameters passed to this function, the current scheduler object
+   * will be created using the structure of a regular competition.
+   *
+   * @param	int	$num_time_blocks_sat	The number of time blocks on saturday.
+   * @param	int	$num_time_blocks_sun	The number of time blocks on sunday.
+   * @param	int	$time_block_duration	The amount of time allocated to each timeblock.
+   * @param	int	$num_concurrent_sections_sat	The number of sections/timeblock on saturday.
+   * @param	int	$num_concurrent_sections_sun	The number of sections/timeblock on sunday.
+   * @param	int	$num_master_sections_sat	The number of master-class sections on saturday.
+   * @param	int	$num_master_sections_sun	The number of master-class sections on sunday.
+   *
+   * @since 1.0.0
+   * @author KREW
+   */
+  public function create_normal_competition($num_time_blocks_sat,
+                                            $num_time_blocks_sun,
+                                            $time_block_duration,
+                                            $num_concurrent_sections_sat,
+                                            $num_concurrent_sections_sun,
+                                            $num_master_sections_sat,
+                                            $num_master_sections_sun) {
+    // ensure the current scheduler object is for a regular competition
+    if ($this->competition_type !== REGULAR_COMP) {
+      return;
+    }
+
+    // create the time blocks with their concurrent sections for saturday
+    $this->days[SAT] = new SplFixedArray($num_time_blocks_sat);
+    for ($i = 0; $i < $num_time_blocks_sat; $i++) {
+      $this->days[SAT][$i] = new TimeBlock($num_concurrent_sections_sat, $time_block_duration);
+    }
+
+    // designate some of the sections on saturday for master-class students
+    while ($num_master_sections_sat > 0) {
+      for ($i = ($num_time_blocks_sat - 1); $i >= ($num_time_blocks_sat / 2); $i--) {
+        if ($num_master_sections_sat > 0 && $this->days[SAT][$i]->assign_section_to_master()) {
+          $num_master_sections_sat--;
+        }
       }
     }
 
-    /*
-    echo "Just created a scheduler object.";
-    wp_die(print_r($this->days));
-    */
+    // create the time blocks with their concurrent sections for sunday
+    $this->days[SUN] = new SplFixedArray($num_time_blocks_sun);
+    for ($i = 0; $i < $num_time_blocks_sun; $i++) {
+      $this->days[SUN][$i] = new TimeBlock($num_concurrent_sections_sun, $time_block_duration);
+    }
+
+    // designate some of the sections on sunday for master-class students
+    while ($num_master_sections_sun > 0) {
+      for ($i = ($num_time_blocks_sun - 1); $i >= ($num_time_blocks_sun / 2); $i--) {
+        if ($num_master_sections_sun > 0 && $this->days[SUN][$i]->assign_section_to_master()) {
+          $num_master_sections_sun--;
+        }
+      }
+    }
+  }
+
+  /**
+   * This function wil create the structure for the command performance.
+   *
+   * Using the parameters passed to this function, a new scheduler object will
+   * be created for a command performance.
+   *
+   * @param	int	$num_time_blocks	The number of time blocks for command performance.
+   * @param	int	$time_block_duration	The amount of time allocated to each timeblock.
+   *
+   * @since 1.0.0
+   * @author KREW
+   */
+  public function create_command_performance($num_time_blocks, $time_block_duration) {
+    // ensure the current scheduler object is for a regular competition
+    if ($this->competition_type !== COMMAND_COMP) {
+      return;
+    }
+
+    // create the time blocks with their concurrent sections (one) for command performance
+    $this->days[COMMAND] = new SplFixedArray($num_time_blocks);
+    for ($i = 0; $i < $num_time_blocks_sat; $i++) {
+      $this->days[COMMAND][$i] = new TimeBlock(1, $time_block_duration);
+    }
   }
 
   /**
