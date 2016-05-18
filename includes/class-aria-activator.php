84548d855b73ef97967569017357a7c72df38c4e
Wesley Kepke <wesleykepke@nevada.unr.edu>
2016-05-17 20:39:41 -0700

    Added function to create score input form to activator

--- includes/class-aria-activator.php
+++ includes/class-aria-activator.php
@@ -56,6 +56,7 @@ class ARIA_Activator {
     require_once("class-aria-teacher-upload.php");
     //require_once(ARIA_ROOT . "/admin/scheduler/doc-generator.php");
 		require_once(ARIA_ROOT . "/admin/scheduler/modify-schedule.php");
+    require_once(ARIA_ROOT . "/admin/scheduler/score-input.php");
 		require_once("class-aria-resend-email-form.php");
     ARIA_Create_Competition::aria_create_competition_activation();
     ARIA_Music::aria_create_music_upload_form();
@@ -64,5 +65,6 @@ class ARIA_Activator {
     //Doc_Generator::aria_create_doc_gen_page();
 		ARIA_Resend_Email::aria_create_resend_teacher_email_form();
 		Modify_Schedule::aria_create_modify_schedule_page();
+    Score_Input::aria_create_score_input_page();
   }
 }
